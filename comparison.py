bfc9c3376c21a6f6c235cbc9960abd728223207a
Capra <riccardocapra0@gmail.com>
2020-07-13 10:47:36 +0200

    Comparison.py fixed

--- comparison.py
+++ comparison.py
@@ -4,19 +4,28 @@ import numpy as np
 import time
 import csv
 if __name__ == "__main__":
-    dimension_array = np.linspace(25, 700, num=28)
+    
+    dimension_array = np.linspace(25, 700, num=4)
     time_array_library_dct = []
     time_array_my_dct = []
+
     for dimension in dimension_array:
-        print("Computing the dimension: " + str(dimension))
+        print("Calcolo dimensioni: " + str(dimension))
         matrix = np.random.rand(int(dimension), int(dimension))
+
         start_time = time.time()
+        #print(start_time);
         solved_library_dct2 = get_2D_dct(matrix)
-        time_array_library_dct.append(time.time() - start_time)
+        end_time = time.time() - start_time;
+        time_array_library_dct.append(end_time)
+        #print(end_time);
+
         start_time = time.time()
         solved_my_dct2 = dct2_my_implementation(matrix)
-        time_array_my_dct.append(time.time() - start_time)
-    with open('data.txt', 'w') as w:
-        writer = csv.writer(w, delimiter='\t')
-        writer.writerows(zip(dimension_array, time_array_library_dct,
-        time_array_my_dct))
+        end_time = time.time() - start_time;
+        time_array_my_dct.append(end_time)
+
+    with open('data.csv', 'w') as w:
+        writer = csv.writer(w, delimiter=';')
+        writer.writerows(zip(dimension_array, time_array_library_dct,time_array_my_dct))
+#        writer.writerows(zip(dimension_array, time_array_library_dct))
